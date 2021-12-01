e0b4ae139d3615d34ebeb83b9cc18222af454de2
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-01 16:07:27 +0100

    full icon attributions:

--- src/Pages/Layout/Footer.tsx
+++ src/Pages/Layout/Footer.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 
 /*
-    also add findicons.com/
     Icons used:
-    food: https://www.flaticon.com/premium-icon/restaurant_685301
+        food: https://www.flaticon.com/premium-icon/restaurant_685301
+        vista: https://www.flaticon.com/premium-icon/mountain_2404113
+        urbex: https://www.flaticon.com/free-icon/bank_2780776
+        danger: https://www.flaticon.com/premium-icon/dangerous_2012485
+        fuel: https://www.flaticon.com/free-icon/gas-station_483497
+        other: https://www.flaticon.com/premium-icon/more-information_2740648
 
 */
 
