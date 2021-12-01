825ed871b96b21562ccb15080f72c07bdc8cd3d7
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-01 18:09:16 +0100

    replaced icons

--- src/Components/MapConstants.tsx
+++ src/Components/MapConstants.tsx
@@ -1,8 +1,8 @@
 export const mapIconUrls = {
-    "FOOD": process.env.PUBLIC_URL + '/maps_icons/magazines.png',
-    "VISTA": process.env.PUBLIC_URL + '/maps_icons/magazines.png',
-    "URBEX": process.env.PUBLIC_URL + '/maps_icons/magazines.png',
-    "DANGER": process.env.PUBLIC_URL + '/maps_icons/magazines.png',
-    "FUEL": process.env.PUBLIC_URL + '/maps_icons/magazines.png',
-    "OTHER": process.env.PUBLIC_URL + '/maps_icons/magazines.png',
+    "FOOD": process.env.PUBLIC_URL + '/maps_icons/food.png',
+    "VISTA": process.env.PUBLIC_URL + '/maps_icons/vista.png',
+    "URBEX": process.env.PUBLIC_URL + '/maps_icons/urbex.png',
+    "DANGER": process.env.PUBLIC_URL + '/maps_icons/danger.png',
+    "FUEL": process.env.PUBLIC_URL + '/maps_icons/fuel.png',
+    "OTHER": process.env.PUBLIC_URL + '/maps_icons/other.png',
 }
