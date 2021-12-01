41a09eabc3f72b4c2b91cbf6c82f8b616c9f459a
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-01 19:05:11 +0100

    MotoRoutePOI list initial layout

--- src/Components/MotoRouteDetails/MotoRoutePOIsCard.tsx
+++ src/Components/MotoRouteDetails/MotoRoutePOIsCard.tsx
@@ -12,7 +12,24 @@ const MotoRoutePOIsCard = (props: MotoRoutePOIsCardProps) => {
     return (
         <Fragment>
 
-            POIS card
+            <div className="list-group">
+                <a href="#?poi" className="list-group-item list-group-item-action flex-column align-items-start">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-2">
+                                <img src={ process.env.PUBLIC_URL + '/maps_icons/food.png'} />
+                            </div>
+                            <div className="col-md-10">
+
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h5 className="mb-1">List group item bandit</h5>
+                                </div>
+                                <p className="mb-1">Donec id es2lit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
 
         </Fragment>
     )
