966c88b839e0b2462bf46449895e151db5e7f150
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-03 20:29:09 +0100

    fix mobile issues

--- src/Components/MotoRoute/MotoRouteAuthor.tsx
+++ src/Components/MotoRoute/MotoRouteAuthor.tsx
@@ -16,14 +16,14 @@ const MotoRouteAuthor = () => {
             {/* make this a link to authors page */}
             <a href="/#" className="list-group-item list-group-item-action flex-column align-items-start">
                 <div className="row">
-                    <div className="col-md-3">
+                    <div className="col-3">
                         <div className="profile-pic-wrapper">
                             <ProfilePicture 
                                 imgPath="https://thumbs.mugshots.com/gallery/images/b4/86/LAWRENCE-SULLIVAN-mugshot-46777547-hidden-c2fa0003.jpeg.400x800.jpg"
                             />
                         </div>
                     </div>
-                    <div className="col-md-9 author-info">
+                    <div className="col-9 author-info">
                         <h1>John Kowalski</h1>
                         <p>Joined: October 2021</p>
                         <p>Routes added: 12</p>
