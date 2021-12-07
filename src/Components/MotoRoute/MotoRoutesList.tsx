fb916417e76b57f66b257361fa5ed818734845ef
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2021-12-07 14:17:24 +0100

    temporary fix moto route list shrink

--- src/Components/MotoRoute/MotoRoutesList.tsx
+++ src/Components/MotoRoute/MotoRoutesList.tsx
@@ -22,11 +22,11 @@ const MotoRoutesList = (props: MotoRoutesListProps) => {
                     { motoRoutesList.map((route) => 
                         <Link key={`moto-list-item-${route._id}`} to={`/routes/${route._id}/details`} className="moto-routes-list-item list-group-item list-group-item-action">
                             <div className="row">
-                                <div className="col-sm-4">
+                                <div className="map-thumbnail-col">
                                     <img src={ process.env.PUBLIC_URL + '/samples/map1.png' } alt="map of the route" />
                                 </div>
 
-                                <div className="col-sm-8">
+                                <div className="col-sm-7">
                                     <p>{ route.title }</p>
                                     <p className="moto-routes-list-item-description">{ route.description }</p>
                                 </div>
