3a85d56657d427d59f9db822dc4d8fc696e6c6b4
Mikolaj Panasiuk <m.panasiuk.14@outlook.com>
2022-01-07 18:07:18 +0100

    prepare for adding pois

--- src/Pages/MotoRouteEditor.tsx
+++ src/Pages/MotoRouteEditor.tsx
@@ -1,26 +1,55 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import MotoRouteEditorMap from "../Components/MotoRoute/Editor/MotoRouteEditorMap";
 import MotoRouteDetailsEditor from "../Components/MotoRoute/Editor/MotoRouteEditorDetails";
 import { useMatch } from "react-router-dom";
 
+enum addModes {
+    NONE,
+    ADD_WAYPOINT,
+    ADD_POI
+}
+
 const MotoRouteEditor = () => {
 
     const [route, setRoute] = useState<{lat: number, lng: number}[]>([])
+    const [addMode, setAddMode] = useState<addModes>(addModes.NONE)
 
 
-    const urlMatch = useMatch('/routes/editor/:')
-    console.log(urlMatch['*'])
+    const urlMatch = useMatch('/routes/editor/:tab')
+
+    useEffect(() => {
+        switch(urlMatch?.params.tab) {
+            case "details": // On details, same behaviour as on route
+            case "route":
+                setAddMode(addModes.ADD_WAYPOINT)
+                break
+            case "poi":
+                setAddMode(addModes.ADD_POI)
+                break
+            default: 
+                setAddMode(addModes.NONE)
+                break
+        }
+    }, [urlMatch])
 
     const handleMapClick = (e: any) => {
         var lat = e.latLng.lat()
         var lng = e.latLng.lng()
 
+
         // Prevent adding two waypoints in same place. This isn't really possible if done manually
-        let newWaypoint = {lat: lat, lng: lng}
-        if (!route.includes(newWaypoint)) {
-            setRoute([...route, newWaypoint])
+        let newCoords = {lat: lat, lng: lng}
+
+        if (addMode == addModes.ADD_WAYPOINT) {
+            if (!route.includes(newCoords)) {
+                setRoute([...route, newCoords])
+            } else {
+                console.log("Cannot add two waypoints in the same location")
+            }
+        } else if (addMode == addModes.ADD_POI) {
+            console.log("addding poi")
         } else {
-            console.log("Cannot add two waypoints in the same location")
+            throw new Error("No add mode selected")
         }
     }
 
